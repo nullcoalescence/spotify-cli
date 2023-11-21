bd522fcf9c5093b5688094565a6d3498684ebb8c
ben <9029708+nullcoalescence@users.noreply.github.com>
2023-11-20 17:19:50 -0800

    create project and gitignore

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace spotify_cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
